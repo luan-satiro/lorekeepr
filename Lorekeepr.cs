ee46fe03a196ad9f0199c9d714e72f5b26004cc9
luan-satiro <luan.satiro.9@gmail.com>
2026-03-17 22:10:33 -0300

    Initial code

---
+++ Lorekeepr.cs
@@ -0,0 +1,30 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace Lorekeepr;
+
+public class Lorekeepr
+{
+    public static async Task Main()
+    {
+        var _client = new DiscordSocketClient();
+
+        _client.Log += Log;
+
+        var stringPath = @"C:\Users\luans\Downloads\LOREKEEPR TOKEN.txt";
+
+        var token = File.ReadAllText(stringPath);
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+
+    private static Task Log(LogMessage message)
+    {
+        Console.WriteLine(message.ToString());
+        return Task.CompletedTask;
+    }
+}
